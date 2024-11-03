7d8a8de1fba02efd228882c244ed3534510b5ddc
Vitor dos Santos Rodrigues <92825557+VitorSSR@users.noreply.github.com>
2024-11-03 13:07:37 -0300

    Update ContadorMain.vhd

--- ContadorMain.vhd
+++ ContadorMain.vhd
@@ -1,10 +1,10 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity ContadorMain is
 	port(
 	A2,A1,A0,STEP :  in std_logic_vector(3 downto 0);
-	UPDOWN : in std_logic;
+	ld, max_min, clk, updown: in std_logic;
 	Cent,Deze,Unid: out std_logic_vector(3 downto 0)
 	);
 
@@ -16,20 +16,38 @@ architecture main of ContadorMain is
 	Component SomSubBCD is
 		port(
 		A2,A1,A0,STEP :  in std_logic_vector(3 downto 0);
-		UPDOWN : in std_logic;
+		updown : in std_logic;
 		Cent,Deze,Unid: out std_logic_vector(3 downto 0)
 		);
 	end Component;
 
 
 	--Comparador_magnitude_12b_soma
-	Component comparador_mag is
+	Component comparador_mag_soma is
 		port(
 			A_comparacao, B_comparacao: IN std_logic_vector(11 downto 0);
-			AigualB, AmaiorB, AmenorB: OUT std_logic
+			AigualB, AmaiorB_overflow, AmenorB: OUT std_logic
 		);
+
 	end Component;
 
+	--Comparador_magnitude_12b_subtracao
+	Component comparador_mag_subtracao is
+		port(
+			A_comparacao, B_comparacao: IN std_logic_vector(11 downto 0);
+			AigualB, AmaiorB, AmenorB_overflow: OUT std_logic
+		);
+
+	end Component;
+
+
+	Component mux2x1_4b is
+		port (
+        		A, B : in  std_logic_vector(3 downto 0);
+        		chave : in  std_logic;
+        		S : out std_logic_vector(3 downto 0)  
+    		);
+	end Component;
 
 
 	--Filtro Mx/Mn
@@ -43,18 +61,98 @@ architecture main of ContadorMain is
 	end Component;
 
 
-	--Registradores Mx Mn e Step
-	Component circ_reg is
-		port (ld, step, max_min, clk : in bit;
-				a2, a1, a0             : in bit_vector(3 downto 0);
-				mx_reg, mi_reg         : out bit_vector(11 downto 0);
-				st_reg                 : out bit_vector(3 downto 0));
-	end entity Component;
 
 
-	--Registrador de 4 bits
-	Component reg4 is
-		port(d : in bit_vector(3 downto 0);
-			  clk, ld : in bit;
-			  q : out bit_vector(3 downto 0));
+	--Registrador
+
+	Component registrador is
+		port(
+			inR : in std_logic_vector(3 downto 0);
+			clk : in std_logic;
+			outR : out std_logic_vector(3 downto 0)
+		);
 	end Component;
+
+
+	--DecoderBCD
+
+	Component DecoderBCD is
+
+    		Port ( ENTRADA : in STD_LOGIC_VECTOR (3 downto 0);
+           		SAIDA : out STD_LOGIC_VECTOR (6 downto 0)
+		);
+
+	end Component;
+
+
+
+
+
+signal  saida_somador_BCD, max_registrador, min_registrador: std_logic_vector (11 downto 0);
+
+signal  centena,dezena,unidade, saida_mux_min_unidade, saida_mux_min_dezena, saida_mux_min_centena, saida_mux_max_unidade, saida_mux_max_dezena, saida_mux_max_centena, entrada_bcd_centena, entrada_bcd_dezena, entrada_bcd_unidade: std_logic_vector (3 downto 0);
+
+signal saida_bcd_centena, saida_bcd_dezena, saida_bcd_unidade: std_logic_vector(6 downto 0);
+
+
+
+
+begin
+
+
+somador_subtrator: SomSubBCD port map(SAIDA_REGISTRADOR_CLOCK_CENTENA, SAIDA_REGISTRADOR_CLOCK_DEZENA, SAIDA_REGISTRADOR_CLOCK_UNIDADE, SAIDA_REGISTRADOR_STEP,updown,centena,dezena,unidade);
+
+
+saida_somador_BCD(11 downto 8)<= centena;
+
+saida_somador_BCD(7 downto 4) <= dezena;
+
+saida_somador_BCD(3 downto 0) <= unidade;
+
+
+
+comparar_maximo: comparador_mag_soma port map (saida_somador_BCD, SAIDA_REGISTRADOR_MAXIMO, AigualB, AmaiorB_overflow, AmenorB); --COMPARADOR MÁXIMO
+
+comparar_minimo: comparador_mag_subtracao port map (saida_somador_BCD, SAIDA_REGISTRADOR_MINIMO, AigualB, AmaiorB, AmenorB_overflow);  --COMPARADOR MINIMO
+
+
+
+--os 3 mux que pegam o valor minimo
+
+mux_4x1_minimo_unidade: mux2x1_4b port map (saída_somador_BCD(3 downto 0), SAIDA_REGISTRADOR_MINIMO_UNIDADE, AmaiorB_overflow, saida_mux_min_unidade);
+
+mux_4x1_minimo_dezena: mux2x1_4b port map (saída_somador_BCD(7 downto 4), SAIDA_REGISTRADOR_MINIMO_DEZENA , AmaiorB_overflow, saida_mux_min_dezena);
+
+mux_4x1_minimo_centena: mux2x1_4b port map (saída_somador_BCD(11 downto 8), SAIDA_REGISTRADOR_MINIMO_CENTENA , AmaiorB_overflow, saida_mux_min_centena);
+
+
+
+
+--os 3 mux que pegam o valor maximo
+
+mux_4x1_maximo_unidade: mux2x1_4b port map (saída_somador_BCD(3 downto 0), SAIDA_REGISTRADOR_MAXIMO_UNIDADE, AmenorB_overflow, saida_mux_max_unidade);
+
+mux_4x1_maximo_dezena: mux2x1_4b port map (saída_somador_BCD(7 downto 4), SAIDA_REGISTRADOR_MAXIMO_DEZENA , AmenorB_overflow, saida_mux_max_dezena);
+
+mux_4x1_maximo_centena: mux2x1_4b port map (saída_somador_BCD(11 downto 8), SAIDA_REGISTRADOR_MAXIMO_CENTENA , AmenorB_overflow, saida_mux_max_centena);
+
+
+
+--DECODERS BCD
+
+entrada_bcd_centena <= (SAIDA_REGISTRADOR_CLOCK_CENTENA(1), SAIDA_REGISTRADOR_CLOCK_CENTENA(2), SAIDA_REGISTRADOR_CLOCK_CENTENA(3), SAIDA_REGISTRADOR_CLOCK_CENTENA(0));
+
+entrada_bcd_dezena <= (SAIDA_REGISTRADOR_CLOCK_DEZENA(1), SAIDA_REGISTRADOR_CLOCK_DEZENA(2), SAIDA_REGISTRADOR_CLOCK_DEZENA(3), SAIDA_REGISTRADOR_CLOCK_DEZENA(0));
+
+entrada_bcd_unidade <= (SAIDA_REGISTRADOR_CLOCK_UNIDADE(1), SAIDA_REGISTRADOR_CLOCK_UNIDADE(2), SAIDA_REGISTRADOR_CLOCK_UNIDADE(3), SAIDA_REGISTRADOR_CLOCK_UNIDADE(0));
+
+
+
+decoder_bcd_centena: DecoderBCD port map (entrada_bcd_centena, saida_bcd_centena);
+
+decoder_bcd_dezena: DecoderBCD port map (entrada_bcd_dezena, saida_bcd_dezena);
+
+decoder_bcd_unidade: DecoderBCD port map (entrada_bcd_unidade, saida_bcd_unidade);
+
+
+end main;
